dba2860723219ecb3ac78427c30c22225e92c6e8
anutama <imim.amut@gmail.com>
2023-12-02 21:31:47 +0700

    jaa

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector('.slider');
 
-function activate(e) {
+function activate(e) 
+{
   const items = document.querySelectorAll('.item');
   e.target.matches('.next') && slider.append(items[0])
   e.target.matches('.prev') && slider.prepend(items[items.length-1]);
