5eda23810badcadfd58b0fe9de4ee3a90cb4098f
anutama <imim.amut@gmail.com>
2023-12-10 09:56:28 +0700

    cry

--- javascript/jsform.js
+++ javascript/jsform.js
@@ -1,13 +1,8 @@
-const config = {
-  backendUrl: "http://localhost:8000/", // Default backend URL
-};
-const port = 8000;
-
-// Function to validate Firstname and Lastname
+    // Function to validate Firstname and Lastname
 function validateName() {
-  const fullnameInput = document.getElementById("fullname");
-  const names = fullnameInput.value.trim().split(" ");
-  const errorElement = document.getElementById("fullnameError");
+  const nameInput = document.getElementById("name");
+  const names = nameInput.value.trim().split(" ");
+  const errorElement = document.getElementById("nameError");
 
   if (names.length !== 2) {
     errorElement.textContent = "Please enter both your Firstname and Lastname.";
@@ -20,11 +15,11 @@ function validateName() {
 
 // Function to validate Student ID
 function validateStudentID() {
-  const studentIDInput = document.getElementById("studentID");
-  const studentIDPattern = /^\d{10}$/;
-  const errorElement = document.getElementById("studentIDError");
+  const stdInput = document.getElementById("std");
+  const stdPattern = /^\d{10}$/;
+  const errorElement = document.getElementById("stdError");
 
-  if (!studentIDPattern.test(studentIDInput.value)) {
+  if (!stdPattern.test(stdInput.value)) {
     errorElement.textContent = "Please enter a 10-digit Student ID.";
     return false;
   } else {
@@ -49,171 +44,67 @@ function validateEmail() {
   return true;
 }
 
-// Function to validate form inputs on user input
-function validateFormOnInput() {
-  validateName();
-  validateStudentID();
-  validateEmail();
-}
+    // JavaScript เพื่อจัดการการส่งฟอร์มและแสดงข้อมูล
+    const form = document.getElementById('myForm');
+    const displayDiv = document.getElementById('displayData');
 
-// Function to fetch activity types from the backend
-async function fetchActivityTypes() {
-  try {
-    const response = await fetch(`http://${window.location.hostname}:${port}/getActivityType`);
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      console.error("Failed to fetch activity types.");
-      return [];
-    }
-  } catch (error) {
-    console.error("An error occurred while fetching activity types:", error);
-    return [];
-  }
-}
+    // ดึงข้อมูลทั้งหมดจาก localStorage
+    let allData = JSON.parse(localStorage.getItem('allFormData')) || [];
 
-// Function to populate activity types in the select element
-function populateActivityTypes(activityTypes) {
-  const activityTypeSelect = document.getElementById("activityType");
+    // แสดงข้อมูลทั้งหมด
+    displayData();
 
-  for (const type of activityTypes) {
-    const option = document.createElement("option");
-    option.value = type.id;
-    option.textContent = type.value;
-    activityTypeSelect.appendChild(option);
-  }
-}
+    form.addEventListener('submit', function(event) 
+    {
+      event.preventDefault(); // ป้องกันการโหลดหน้าใหม่เมื่อกด submit
 
-// Event listener when the page content has finished loading
-document.addEventListener("DOMContentLoaded", async () => {
-  const activityTypes = await fetchActivityTypes();
-  populateActivityTypes(activityTypes);
-});
+      // ดึงค่าจากฟอร์ม
+      const name = document.getElementById('name').value;
+      const std = document.getElementById('std').value;
+      const email = document.getElementById('email').value;
+      const work = document.getElementById('work').value;
+      const activity = document.getElementById('activity').value;
+      const academic = document.getElementById('academic').value;
+      const semester = document.getElementById('semester').value;
+      const sd = document.getElementById('sd').value;
+      const ed = document.getElementById('ed').value;
+      const location = document.getElementById('location').value;
+      const description = document.getElementById('description').value;
 
-// Function to submit the form
-async function submitForm(event) {
-  event.preventDefault();
+      // บันทึกข้อมูลลงในอาร์เรย์
+      const formData = { name, std, email, work, activity, academic, semester, sd, ed, location, description };
+      allData.push(formData);
 
-  // Validate form inputs before submission
-  if (!validateName() || !validateStudentID() || !validateEmail()) {
-    return;
-  }
+      // บันทึกข้อมูลทั้งหมดลงใน localStorage
+      localStorage.setItem('allFormData', JSON.stringify(allData));
 
-  const startDateInput = document.getElementById("startDate").value;
-  const endDateInput = document.getElementById("endDate").value;
-  const startDate = new Date(startDateInput);
-  const endDate = new Date(endDateInput);
-
-  if (endDate <= startDate) {
-    alert("End datetime should be after the start datetime.");
-    return;
-  }
-
-  // Create the data object to send to the backend
-  const formData = new FormData(event.target);
-  const data = {
-    first_name: formData.get("fullname").split(" ")[0],
-    last_name: formData.get("fullname").split(" ")[1],
-    student_id: parseInt(formData.get("studentID")),
-    email: formData.get("email"),
-    title: formData.get("workTitle"),
-    type_of_work_id: parseInt(formData.get("activityType")),
-    academic_year: parseInt(formData.get("academicYear")) - 543,
-    semester: parseInt(formData.get("semester")),
-    start_date: formData.get("startDate"),
-    end_date: formData.get("endDate"),
-    location: formData.get("location"),
-    description: formData.get("description")
-  };
-
-  console.log(data);
-
-  try {
-    // Send data to the backend using POST request
-    const response = await fetch(`http://${window.location.hostname}:${port}/record`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+      // แสดงข้อมูลทั้งหมด
+      displayData();
     });
 
-    if (response.ok) {
-      const responseData = await response.json();
-      console.log("Form data submitted successfully!");
-
-      // Format JSON data for display
-      const formattedData = Object.entries(responseData.data)
-        .map(([key, value]) => `"${key}": "${value}"`)
-        .join("\n");
-
-      // Display success message with formatted data
-      alert(responseData.message + "\n" + formattedData);
-
-      document.getElementById("myForm").reset();
-    } else {
-      console.error("Failed to submit form data.");
-
-      // Display error message
-      alert("Failed to submit form data. Please try again.");
-    }
-  } catch (error) {
-    console.error("An error occurred while submitting form data:", error);
-  }
-}
-
-// Event listener for form submission
-document.getElementById("myForm").addEventListener("submit", submitForm);
-
-// Event listeners for input validation on user input
-document.getElementById("fullname").addEventListener("input", validateName);
-document
-  .getElementById("studentID")
-  .addEventListener("input", validateStudentID);
-document.getElementById("email").addEventListener("input", validateEmail);
-
-// Event listener for location input validation on user input
-document.getElementById("location").addEventListener("input", validateLocation);
-document.addEventListener('DOMContentLoaded', function () {
-document.getElementById('resetButton').addEventListener('click', function () {
-    document.getElementById('myForm').reset();
-});
-
-});
-
-//submit
-document.addEventListener('DOMContentLoaded', function () {
-const form = document.getElementById('myForm');
-form.addEventListener('Submit', function (e) {
-    e.preventDefault(); // Prevent the default form submission
-
-    // Fetch all the form data
-    const formData = new FormData(form);
-    const values = {};
-    for (const [key, value] of formData.entries()) {
-        values[key] = value;
-    }
-
-    // Display the values
-    displayValues(values, 'custom-output');
+    // จัดการกรณีรีเฟรชหน้า
+    window.addEventListener('beforeunload', function() {
+      // ล้างข้อมูลที่ถูกบันทึกเมื่อรีเฟรช
+      localStorage.removeItem('allFormData');
     });
-    function displayValues(data) {
-      const container = document.createElement('div');
-      container.className = 'custom-output';
-  
-      const heading = document.createElement('h2');
-      heading.textContent = '˙ᘧ ͜ ˙ ♡ This is my activity ˙ᘧ ͜ ˙ ♡';
-      heading.className = 'activity-heading';
-      container.appendChild(heading);
-  
-      for (const key in data) {
-          const p = document.createElement('p');
-          p.textContent = `${key}: ${data[key]}`;
-          container.appendChild(p);
-      }
 
-      const formContainer = document.querySelector('.form-container');
-      formContainer.appendChild(container);
-  }
-})
+    function displayData() {
+      // แสดงข้อมูลทั้งหมด
+      displayDiv.innerHTML = '<p></p>';
+
+      allData.forEach((data) => {
+        displayDiv.innerHTML += `
+          <p>Name: ${data.name}</p>
+          <p>StudentID: ${data.std}</p>
+          <p>Email: ${data.email}</p>
+          <p>Work: ${data.work}</p>
+          <p>Activity: ${data.activity}</p>
+          <p>Academic: ${data.academic}</p>
+          <p>Semester: ${data.semester}</p>
+          <p>Start Date: ${data.sd}</p>
+          <p>End Date: ${data.ed}</p>
+          <p>Location: ${data.location}</p>
+          <p>Description: ${data.description}</p>
+        `;
+      });
+    }
